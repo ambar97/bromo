7accd8c3aeafaf5e939d5bf16319f8b2ad642b70
biem27 <abiemassyikin@gmail.com>
2019-11-09 21:32:31 +0700

    template admin fix

---
+++ application/views/admin/side/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+        <div class="container-fluid">
+          <nav class="float-left">
+            <ul>
+              
+            </ul>
+          </nav>
+          <div class="copyright float-right">
+            &copy;
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com" target="_blank">Creative Tim Polije</a> for a better web.
+          </div>
+        </div>
+      </footer>
+    </div>
+  </div>
