30f99a1dc74b4803cd69ff9607cd2fbb72ec2eaa
Biem <Abiemassyikin@gmail.com>
2019-11-12 02:01:48 +0700

    all

--- application/views/user/v_contact.php
+++ application/views/user/v_contact.php
@@ -1,43 +1,36 @@
 <?php $this->load->view("user/side/head"); ?>
  <?php $this->load->view("user/side/navbar"); ?>
 <div class="row page-title">
-		<div class="container clear-padding text-center flight-title">
-			<h3>CONTACT US</h3>
-			<h4 class="thank">Let's Get Connected</h4>
-		</div>
+	<div class="container clear-padding text-center flight-title">
+		<h3>CONTACT US</h3>
+		<h4 class="thank">Let's Get Connected</h4>
 	</div>
-	<!-- END: PAGE TITLE -->
-	
-	<!-- START: CONTACT-US -->
-	<?php foreach ($contact->result() as $h): ?>
-	<div class="row contact-address">
+</div>
+	<!-- <div class="row"> -->
+	<div class="row about-intro">
 		<div class="container clear-padding">
-			<div class="text-center">
-				<h2><?php echo $h->judul ?></h2>
-				<h5><?php echo $h->isi ?></h5>
-				<div class="space"></div>
-				<div class="col-md-4 col-sm-4">
-					<i class="fa fa-map-marker"></i>
-					<p><?php echo $h->alamat ?></p>
+			<div class="col-md-6 col-sm-6 contact-form">
+				<h2>Contact Here</h2>
+				<h4></h4>
+				<div class="row">
+					<?php foreach ($contact->result() as $h): ?>
+					<div class="col-md-6">
+						<p class="thank"><i class="fa fa-envelope-o"></i><a href="" style="color: black"> <?php echo $h->email ?></a></p>
+						<p class="thank"><i class="fa fa-phone"> </i><a href="" style="color: black"><?php echo $h->noTelp ?></a></p>
+						<p class="thank"><i class="fa fa-whatsapp"> </i><a href="" style="color: black"><?php echo $h->noWa ?></a></p>
+						<p class="thank"><i class="fa fa-map-marker"></i><a href="" style="color: black">  <?php echo $h->alamat ?></a></p>
+
+					</div>
+					<div class="col-md-6">
+						<p class="thank"><i class="fa fa-instagram"> </i><a href="" style="color: black">  <?php echo $h->ig ?></a></p>
+						<p class="thank"><i class="fa fa-facebook"> </i><a href="" style="color: black"> <?php echo $h->facebook ?></a></p>
+						<p class="thank"><i class="fa fa-phone"> </i><a href="" style="color: black">  <?php echo $h->idLine ?></a></p>
+						
+					</div>
+					<hr>
+					<?php endforeach ?>
+
 				</div>
-				<div class="col-md-4 col-sm-4">
-					<i class="fa fa-envelope-o"></i>
-					<p><a href="mailto:<?php echo $h->email ?>"><?php echo $h->email ?></a></p>
-				</div>
-				<div class="col-md-4 col-sm-4">
-					<i class="fa fa-phone"></i>
-					<p><?php echo $h->noTelp ?></p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<?php endforeach ?>
-	<!-- END: CONTACT-US -->
-	<!-- START: MAP & CONTACT FORM -->
-	<div class="row" style="background-color: white">
-		<div class="container clear-padding">
-			<div class="col-md-6 col-sm-6">
-				<iframe class="contact-map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJG1usnet4BTkRzQqb_Ys-JOg&amp;key=AIzaSyB6hgZM-ruUqTPVUjXGUR-vv7WRqc4MXjY"></iframe>
 			</div>
 			<div class="col-md-6 col-sm-6 contact-form">
 				<div class="col-md-12">
@@ -65,6 +58,16 @@
 					</div>
 				</form>
 			</div>
+		</div>
+	</div>
+	<!-- END: CONTACT-US -->
+	<!-- START: MAP & CONTACT FORM -->
+	<div class="row" style="background-color: white">
+		<div class="container clear-padding">
+			<div class="col-md-12 col-sm-12">
+				<iframe class="contact-map" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJG1usnet4BTkRzQqb_Ys-JOg&amp;key=AIzaSyB6hgZM-ruUqTPVUjXGUR-vv7WRqc4MXjY"></iframe>
+			</div>
+			
 		</div>
 	</div>
 		<?php $this->load->view("user/side/footer"); ?>
