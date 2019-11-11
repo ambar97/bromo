4fa144d4c064c8567c254ac04020045c7e9f537e
cnjonathan <cnjonathan.cnj@gmail.com>
2019-11-11 16:14:37 +0700

    navbar bento

--- application/controllers/admin/Hotel.php
+++ application/controllers/admin/Hotel.php
@@ -13,6 +13,22 @@ class Hotel extends CI_Controller {
 		$this->load->model('M_model');
 		$id = $this->uri->segment(4);
 		$data['hotel']=$this->M_model->selectwhere('hotel', array('idHotel'=>$id));
+
+		$config['upload_path']          = base_url('gallery/hotel');
+		$config['allowed_types']        = 'gif|jpg|png';
+
+		$this->load->library('upload', $config);
+
+		if ( $this->upload->do_upload(''))
+		{
+						$data = $this->upload->data();
+						$name_file=$data['file_name'];
+						$update_data['gambar_mobil'] = $name_file;
+						$id_mobil=$this->input->post('id_mobil');
+						$this->suzuki_model->update('mobil', $update_data, array('id_mobil'=>$id_mobil));
+						header('location:'.base_url('admin/Product/editData/'.$id_mobil));
+		}
+
 		$this->load->view('admin/edit/hotel', $data);
 	}
 
