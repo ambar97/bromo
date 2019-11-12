8b5de99a3ec57441a6dc6af006e4a23d78aeff58
cnjonathan <cnjonathan.cnj@gmail.com>
2019-11-12 10:38:15 +0700

    paket

--- application/controllers/admin/PaketWisata.php
+++ application/controllers/admin/PaketWisata.php
@@ -1,1 +1,95 @@
-PaketWisata.php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PaketWisata extends CI_Controller {
+
+	public function index(){
+		$this->load->model('M_model');
+		$data['paket']=$this->M_model->select('paket_wisata');
+		$this->load->view('admin/v_paketwisata', $data);
+	}
+
+	public function editPaket(){
+		$this->load->model('M_model');
+		$id = $this->uri->segment(4);
+		$data['Paket'] = $this->M_model->selectwhere('Paket', array('idpaket_wisata'=>$id));
+		$data['gallery'] = $this->M_model->select('galery', array('wisata_idwisata'=>$id, 'hotel_idhotel'=>NULL));
+		$this->load->view('admin/edit/Paket', $data);
+	}
+
+	public function prosesEditPaket(){
+    $this->load->model('M_model');
+		$id = $this->input->post('idPaket');
+		$data['idPaket']=$id;
+		$data['nama_Paket']=$this->input->post('nama_Paket');
+		$data['rating']=$this->input->post('rating');
+		$data['deskripsi']=$this->input->post('deskripsi');
+		$data['harga']=$this->input->post('harga');
+		$this->M_model->update('Paket', $data, array('idPaket'=>$id));
+
+		$config['upload_path']          = 'gallery/Paket';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['file_name']						= $id.$data['nama_Paket'];
+
+		$this->load->library('upload', $config);
+		if ( $this->upload->do_upload('gambar')){
+						$data = $this->upload->data();
+						$name_file=$data['file_name'];
+						$gambar_data['gambar'] = $name_file;
+						$gambar_data['Paket_idPaket'] = $id;
+						$this->M_model->insert('galery', $gambar_data);
+		}
+		redirect(base_url('admin/Paket/editPaket/'.$id));
+	}
+
+	public function prosesHapusPaket(){
+		$this->load->model('M_model');
+		$id = $this->uri->segment(4);
+		$deletebyidPaket = array('idPaket'=>$id);
+		$deletebyidgambar = array('Paket_idPaket'=>$id);
+		$this->M_model->delete($deletebyidgambar, 'galery');
+		$this->M_model->delete($deletebyidPaket, 'Paket');
+		return redirect(base_url('admin/Paket'));
+	}
+
+	public function prosesHapusGambar(){
+		$this->load->model('M_model');
+		$id = $this->uri->segment(4);
+		$deletebyname = array('gambar'=>$id);
+		// unlink(base_url().'gallery/Paket/'.$deletebyname);
+		$this->M_model->delete($deletebyname, 'galery');
+		return redirect(base_url('admin/Paket/'));
+	}
+
+	public function insertPaket(){
+		$this->load->view('admin/insert/Paket');
+	}
+
+	public function prosesInsertPaket(){
+		$this->load->model('M_model');
+		$data['idPaket']=$id;
+		$data['nama_Paket']=$this->input->post('nama_Paket');
+		$data['rating']=$this->input->post('rating');
+		$data['deskripsi']=$this->input->post('deskripsi');
+		$data['harga']=$this->input->post('harga');
+		$this->M_model->insert('Paket', $data);
+
+		$config['upload_path']          = 'gallery/Paket';
+		$config['allowed_types']        = 'gif|jpg|png';
+
+		$this->load->library('upload', $config);
+		if ( $this->upload->do_upload('gambar')){
+						$data = $this->upload->data();
+						$name_file=$data['file_name'];
+						$gambar_data['gambar'] = $name_file;
+						$id=$this->db->insert_id();
+						$gambar_data['Paket_idPaket'] = $id;
+						$this->M_model->insert('galery', $gambar_data);
+		}
+		redirect(base_url('admin/Paket'));
+	}
+
+	public function coba(){
+		$this->load->view('admin/coba');
+	}
+}
---
+++ application/views/admin/v_paketwisata.php
@@ -0,0 +1,85 @@
+<?php $this->load->view("admin/side/head"); ?>
+<?php $this->load->view("admin/side/navbar"); ?>
+
+<div class="header bg-primary pb-6">
+ <div class="container-fluid">
+   <div class="header-body">
+     <div class="row align-items-center py-4">
+       <div class="col-lg-6 col-7">
+         <h6 class="h2 text-white d-inline-block mb-0">Paket Wisata</h6>
+         <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+           <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+             <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+             <li class="breadcrumb-item"><a href="#">Paket Wisata</a></li>
+             <!-- <li class="breadcrumb-item active" aria-current="page">Default</li> -->
+           </ol>
+         </nav>
+       </div>
+     </div>
+     <!-- Card stats -->
+
+   </div>
+ </div>
+</div>
+<!-- Page content -->
+<div class="container-fluid mt--6">
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header card-header-primary">
+              <h4 class="card-title ">Daftar Paket</h4>
+              <a class="btn btn-primary text-white" href="<?php echo base_url('admin/Hotel/insertHotel'); ?>">Tambah Paket</a>
+            </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table">
+                  <thead class=" text-primary">
+                    <th>
+                      ID
+                    </th>
+                    <th>
+                      Nama Paket
+                    </th>
+                    <th>
+                      Harga
+                    </th>
+                    <th>
+                      Aksi
+                    </th>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($paket->result() as $h): ?>
+                      <tr>
+                        <td>
+                          <?php echo $h->idpaket_wisata; ?>
+                        </td>
+                        <td>
+                          <?php echo $h->nama_paket; ?>
+                        </td>
+                        <td class="text-primary">
+                          <?php echo number_format($h->harga); ?>
+                        </td>
+                        <td>
+                          <a class="btn btn-warning" href="<?php echo base_url('admin/PaketWisata/editPaket/'.$h->idpaket_wisata); ?>">Edit</a>
+                          <a class="btn btn-danger" href="<?php echo base_url('admin/PaketWisata/prosesHapusPaket/'.$h->idpaket_wisata); ?>">Hapus</a>
+                        </td>
+                      </tr>
+                    <?php endforeach; ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ <!-- Footer -->
+
+ <?php $this->load->view('admin/side/footer') ?>
+ <?php $this->load->view('admin/side/js') ?>
+ <!-- Argon JS -->
+ <script src="<?php echo base_url()?>master/assets/js/argon.min9f1e.js?v=1.1.0"></script>
+</body>
+</html>
