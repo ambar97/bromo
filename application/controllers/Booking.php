1cb77eb6ff6460bfe4375c121c254eafe1610d88
ambar97 <abiemassyikin@gmail.com>
2019-11-15 19:58:30 +0700

    Cart Ok

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -8,6 +8,8 @@ function __construct(){
 		parent::__construct();
 	}
 public function bookingW(){
+
+	// die(var_dump($this->cart->contents()));
 		$this->load->view('user/booking/single');
 	}
 	public function destroycart(){
@@ -18,25 +20,27 @@ public function bookingW(){
 		$data = array('rowid' => $rowid,
                               'qty' => 0);
 		$this->cart->update($data);
-		// die(var_dump($this->cart->contents()));
 		redirect(base_url('Booking/bookingW'));
 	}
 	public function bookingST(){
 
 		$dataaa = $this->input->post('idwis');
-		$usd = $this->cart->contents();
-		if ($usd['id'] == $dataa; ) {
-
+		$usd['data']= $this->cart->contents();
+		// $this->cart->destroy();
+		if ($usd['id'] == $dataaa ) {
+			redirect(base_url('Booking/bookingW/'));
 		} else {
 			$data = array('id'      => $this->input->post('idwis'),
 	                'qty'     => 1,
 	                'price'   => $this->input->post('harga'),
 	                'name'    => $this->input->post('nama'),
 	                'gambar' =>  $this->input->post('foto'));
+									$this->cart->insert($data);
+									redirect(base_url('Booking/bookingW/'));
 		}
-		$this->cart->insert($data);
-		// $this->cart->destroy();
-		// die(var_dump($this->cart->contents()));
-		redirect(base_url('Booking/bookingW/'));
+
+
+
+
 	}
 }
